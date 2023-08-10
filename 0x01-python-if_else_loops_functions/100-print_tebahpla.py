c26686660a8fc2302b5d166f8a51594924df2fff
anoksimon <chunok4realg2@gmail.com>
2023-08-09 22:21:20 -0700

    python project 0x01

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -5,9 +5,3 @@ for i in range(ord('z'), ord('a') - 1, -1):
     else:
         diff = 32
     print('{}'.format(chr(i - diff)), end='')
-101-remove_char_at.py:
-#!/usr/bin/python3
-def remove_char_at(str, n):
-    if n < 0:
-        return (str)
-    return (str[:n] + str[n+1:])
