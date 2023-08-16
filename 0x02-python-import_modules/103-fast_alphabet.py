7a7148ca01588978a9d140c9fc03f75a92a478c3
anoksimon <chunok4realg2@gmail.com>
2023-08-16 15:27:21 -0700

    0x02-python import modules

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import string
+print(string.ascii_uppercase)
+
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+
